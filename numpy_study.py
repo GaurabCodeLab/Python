436d69b5fb5339dad2088120983c7659f7f4b631
GaurabCodeLab <kumargaurab56@gmail.com>
2026-02-18 14:31:33 +0530

    code added

--- numpy_study.py
+++ numpy_study.py
@@ -91,8 +91,61 @@ import numpy as np
 # x[0] = 31
 # print(arr)  # [31  2  3  4  5]
 # print(x)  # [31  2  3  4  5]
-arr = np.array([1, 2, 3, 4, 5])
-x = arr.copy()
-y = arr.view()
-print(x.base)  # None
-print(y.base)  # [1 2 3 4 5]
+# arr = np.array([1, 2, 3, 4, 5])
+# x = arr.copy()
+# y = arr.view()
+# print(x.base)  # None
+# print(y.base)  # [1 2 3 4 5]
+
+# NumPy Array Shape:-
+# arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+# print(arr.shape)  # (2, 4)
+# arr = np.array([1, 2, 3, 4], ndmin=5)
+# print(arr)  # [[[[[1 2 3 4]]]]]
+# print(arr.shape)  # (1, 1, 1, 1, 4)
+# Integers at every index tells about the number of elements the corresponding dimension has.
+# In the example above at index-4 we have value 4, so we can say that 5th ( 4 + 1 th) dimension has 4 elements.
+
+# NumPy Array Reshaping:-
+# Reshaping means changing the shape of an array.
+# Reshape From 1-D to 2-D:-
+# arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+# newarr = arr.reshape(4, 3)
+# print(newarr)  # [[ 1  2  3], [ 4  5  6],  [ 7  8  9],  [10 11 12]]
+# Reshape From 1-D to 3-D:-
+# arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+# newarr = arr.reshape(2, 3, 2)
+# print(newarr)  #[[[ 1  2]   [ 3  4]   [ 5  6]]  [[ 7  8]   [ 9 10]   [11 12]]]
+# We Reshape Into any Shape as long as the elements required for reshaping are equal in both shapes.
+# We can reshape an 8 elements 1D array into 4 elements in 2 rows 2D array but we cannot reshape it into a 3 elements 3 rows 2D array as that would require 3x3 = 9 elements.
+# arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+# reshaped_arr = arr.reshape(2, 4)
+# print(reshaped_arr)  # [[1 2 3 4]  [5 6 7 8]]
+# print(reshaped_arr.base)  # [1 2 3 4 5 6 7 8]
+# array reshape returns view
+# You are allowed to have one "unknown" dimension.
+# Meaning that you do not have to specify an exact number for one of the dimensions in the reshape method.
+# Pass -1 as the value, and NumPy will calculate this number for you.
+# arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+# newarr = arr.reshape(2, 2, -1)
+# print(newarr)  # [[[1 2]   [3 4]]  [[5 6]   [7 8]]]
+# We can not pass -1 to more than one dimension.
+# Flattening array means converting a multidimensional array into a 1D array.
+# We can use reshape(-1) to do this.
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+newarr = arr.reshape(-1)
+print(newarr)  # [1 2 3 4 5 6]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
